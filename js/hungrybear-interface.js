5820d834a6f43af6cceec4f19ac25508cfc1b0eb
Edgar Hernandez and Matt Jackson <matt@>
2018-02-12 15:01:34 -0800

    Add clear variable in hungrybear-interface

--- js/hungrybear-interface.js
+++ js/hungrybear-interface.js
@@ -1,19 +1,46 @@
 import { bear } from './../js/hungrybear.js';
 
 let fuzzy = bear;
-fuzzy.foodLevel = 10;
-
+//fuzzy.foodLevel = 10;
+//fuzzy.sleepLevel = 10;
+//fuzzy.moodLevel = 10;
 $(document).ready(function() {
-
   fuzzy.setHunger();
+  fuzzy.setSleep();
+  fuzzy.setMood();
   let timer = setInterval(() => {
+    if(fuzzy.didYouGetEaten()){
+    alert("Feed me now");
     $("#food").empty();
-    $("#food").append(`${fuzzy.foodLevel}`);
+    $("#sleep").empty();
+    $("#mood").empty();
+  }
+    $("#food").empty().append(`${fuzzy.foodLevel}`);
+    $("#sleep").empty().append(`${fuzzy.sleepLevel}`);
+    $("#mood").empty().append(`${fuzzy.moodLevel}`);
+  }, 1000);
+
+  let clear = setInterval(() => {
+    if(fuzzy.didYouGetEaten()) {
+      clearInterval(timer);
+    }
   }, 1000);
 
   $("#foodButton").click(function(event){
-        event.preventDefault();
-        fuzzy.feed();
-      })
+    event.preventDefault();
+    fuzzy.feed();
+    });
+
+  $("#sleepButton").click(function(event){
+    event.preventDefault();
+    fuzzy.putToBed();
+    });
+
+  $("#playButton").click(function(event){
+    event.preventDefault();
+    fuzzy.play();
+    });
+
+
 
 });
--- js/hungrybear.js
+++ js/hungrybear.js
@@ -1,5 +1,7 @@
 export let bear = {
   foodLevel: 10,
+  sleepLevel: 10,
+  moodLevel: 10,
   setHunger: function() {
     const hungerInterval = setInterval(() => { //bear.setHunger for results
       this.foodLevel--;
@@ -9,8 +11,26 @@ export let bear = {
       }
     }, 1000);
   },
+  setSleep: function() {
+    const sleepInterval = setInterval(() => { //bear.setHunger for results
+      this.sleepLevel--;
+      if (this.didYouGetEaten() == true) {
+        clearInterval(sleepInterval);
+        return "You got eaten!";
+      }
+    }, 1000);
+  },
+  setMood: function() {
+    const moodInterval = setInterval(() => { //bear.setHunger for results
+      this.moodLevel--;
+      if (this.didYouGetEaten() == true) {
+        clearInterval(moodInterval);
+        return "You got eaten!";
+      }
+    }, 1000);
+  },
   didYouGetEaten: function() {
-    if (this.foodLevel > 0) {
+    if (this.foodLevel > 0 && this.sleepLevel > 0 && this.moodLevel > 0) {
       return false;
     } else {
       return true;
@@ -18,7 +38,15 @@ export let bear = {
   },
   feed() {
       this.foodLevel = 10;
-    }
+    },
+
+  putToBed() {
+    this.sleepLevel = 10;
+  },
+
+  play() {
+    this.moodLevel = 10;
+  }
 };
 
 bear.eatSmall = bear.feed(5);
